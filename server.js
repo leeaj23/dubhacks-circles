6393580e691d2d50f90bd47de162323b5849eb14
James <james@jamesssystems.info>
2024-10-12 14:18:11 -0700

    auth0 ALREADY.... thast what im talkign about

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const port = 3000;
+const { auth } = require("express-openid-connect");
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: "http://localhost:3000",
+  clientID: "2xEeiTf21ZTJpXmdPNV5X2dDOEJG81MJ",
+  issuerBaseURL: "https://circlesapp.us.auth0.com",
+  secret: "deda53d80e511a29137f3983e7bc9334b8a078831d215d1f6067ddf989843686",
+};
+
+app.use(auth(config));
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
